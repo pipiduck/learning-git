6bc8555cc72a3a538efaa885dd727b01ca2f88de
pipiduck <67134787+pipiduck@users.noreply.github.com>
2021-09-30 09:09:55 +0800

    A

--- main.c
+++ main.c
@@ -5,7 +5,8 @@
  * @Date: 2021-09-30 08:54:11
  * @Feature:  
  * @LastEditors: wsy
- * @LastEditTime: 2021-09-30 08:56:46
+ * @LastEditTime: 2021-09-30 09:08:54
  * @FilePath: \learning-git\main.c
  */
 printf("init main.c")
+A
